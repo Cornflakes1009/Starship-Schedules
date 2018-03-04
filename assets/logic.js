9d45fe69efcf68240332ef716bd0e7cf16300177
Harold Davidson <harold.davidson89@gmail.com>
2018-03-03 19:01:11 -0600

    switching computers

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
     var config = {
         apiKey: "AIzaSyDIeJKq6Q2c256oB88R0ktLZOtxP49u5jM",
         authDomain: "starshipscheduler.firebaseapp.com",
@@ -16,13 +15,24 @@ $(function () {
     var destination = '';
     var firstShip = '';
     var frequency = '';
-    var cost = '';
-
+    
 
     database.ref().on('child_added', function (childSnapshot) {
-        $('#ship-board').append("<tr class='removable'>" + '<td>' + childSnapshot.val().ship + '</td>' + '<td>' + childSnapshot.val().destination + '</td>' + '<td>' + childSnapshot.val().firstShip + '</td>' + '<td>' + childSnapshot.val().frequency + '</td>' + '<td>' + childSnapshot.val().cost + '</td>' + '</tr>');
-    });
+        $('#ship-board').append("<tr class='removable'>" + '<td>' + childSnapshot.val().ship + '</td>' + '<td>' + childSnapshot.val().destination + '</td>' + '<td>' + childSnapshot.val().frequency + '</td>' + '<td>' + childSnapshot.val().firstShip + '</td>' + '<td>' + childSnapshot.val().minUntilShip + '</td>' + '</tr>');
+        var tr = $("<tr>");
     var firstShipConverted = moment(firstShip, "hh:mm").subtract("1, years");
+    var currentTime = moment();
+    // the time difference between current time and the first train
+    var difference = moment().diff(moment(firstShipConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilShip = frequency - remainder;
+    moment(minUntilShip).minutes();
+    var nextArrival = moment().add(minUntilShip, "minutes").format("hh:mm a");
+    tr.append("<td>" + nextArrival + "</td>");
+    tr.append("<td>" + minUntilShip + "</td>");
+    
+    });
+    
 
     $("#submit-button").on('click', function (event) {
         event.preventDefault();
@@ -30,30 +40,47 @@ $(function () {
         destination = $("#destination").val().trim();
         firstShip = $("#first-ship").val().trim();
         frequency = $("#frequency").val().trim();
-        cost = $('#cost').val().trim();
-        if (ship !== '' && destination !== '' && firstShip !== '' && frequency !== '' && cost !== '') {
+        
+        if (ship !== '' && destination !== '' && firstShip !== '' && frequency !== '') {
             database.ref().push({
                 ship: ship,
                 destination: destination,
                 firstShip: firstShip,
                 frequency: frequency,
-                cost: cost
+                
             });
         } else {
             alert('Please make sure you fill out all fields. Thank you!');
         }
         console.log(frequency);
+        $("#ship").val('');
+        $("#destination").val('');
+        $("#first-ship").val('');
+        $("#frequency").val('');
     })
 
     function removeTableRow(){
        var ask = confirm('Are you sure you want to delete this entry?');
         if(ask === true){
             (this).remove();
+            // need to remove item from database
         return false;
-
         }
-        
     };
+    // var tr = $("<tr>");
+    // var firstShipConverted = moment(firstShip, "hh:mm").subtract("1, years");
+    // var currentTime = moment();
+    // // the time difference between current time and the first train
+    // var difference = moment().diff(moment(firstShipConverted), "minutes");
+    // var remainder = difference % frequency;
+    // var minUntilShip = frequency - remainder;
+    // moment(minUntilShip).minutes();
+    // var nextArrival = moment().add(minUntilShip, "minutes").format("hh:mm a");
+    // tr.append("<td>" + nextArrival + "</td>");
+    // tr.append("<td>" + minUntilShip + "</td>");
+
+
+
     $(document).on("click", ".removable", removeTableRow);
 }); // end of document ready
 
