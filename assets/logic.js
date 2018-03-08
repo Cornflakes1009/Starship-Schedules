414794186592700bbeb2ddfda3690505541b1bce
Harold Davidson <harold.davidson89@gmail.com>
2018-03-07 21:20:56 -0600

    bug fix with the time calculations

--- assets/logic.js
+++ assets/logic.js
@@ -8,20 +8,55 @@ $(function () {
         messagingSenderId: "844519570002"
     };
     firebase.initializeApp(config);
-
     var database = firebase.database();
+
+    database.ref().on('child_added', function (childSnapshot) {
+        $('#ship-board').append("<tr class='removable'>" + '<td>' + childSnapshot.val().ship + '</td>' + '<td>' + childSnapshot.val().destination + '</td>' + '<td>' + childSnapshot.val().frequency + '</td>' + '<td>' + calculateTime(childSnapshot).nextArrival + '</td>' + '<td>' + calculateTime(childSnapshot).minUntilShip + '</td>' + '</tr>');
+    });
+
+
     var ship = "";
     var destination = '';
     var firstShip = $("#first-ship").val();
+
     var frequency = $("#frequency").val().trim();
+    //console.log("freq", frequency);
     var minUntilShip;
 
+    function calculateTime(childSnapShot) {
+        /////
+
+        //var tr = $("<tr>");
+        //console.log('firstShip is ' + firstShip);
+        var firstShipConverted = moment(childSnapShot.val().firstShip, "hh:mm").subtract("1", "years");
+        //console.log("first ship converted is " + firstShipConverted);
+
+        
+        // the time difference between current time and the first train
+        //console.log(firstShipConverted);
+        var difference = moment().diff(moment(firstShipConverted), "minutes");
+        // console.log('first ship ', firstShip);
+        // console.log("the difference is " + difference);
+        var remainder = difference % childSnapShot.val().frequency;
+        console.log("the remainder is " + remainder);
+        minUntilShip = childSnapShot.val().frequency - remainder;
+        //console.log("minutes until ship is " + minUntilShip);
+        moment(minUntilShip).minutes();
+        var nextArrival = moment().add(minUntilShip, "minutes").format("hh:mm a");
+        // tr.append("<td>" + nextArrival + "</td>");
+        // tr.append("<td>" + minUntilShip + "</td>");
+        /////
+        
+        var result = {
+            minUntilShip: minUntilShip,
+            nextArrival: nextArrival
+        }
+        return result;
+    }
 
 
 
-    database.ref().on('child_added', function (childSnapshot) {
-        $('#ship-board').append("<tr class='removable'>" + '<td>' + childSnapshot.val().ship + '</td>' + '<td>' + childSnapshot.val().destination + '</td>' + '<td>' + childSnapshot.val().frequency + '</td>' + '<td>' + childSnapshot.val().nextArrival + '</td>' + '<td>' + childSnapshot.val().minUntilShip + '</td>' + '</tr>');
-    });
+
 
     $("#submit-button").on('click', function (event) {
         event.preventDefault();
@@ -31,24 +66,7 @@ $(function () {
         console.log(firstShip);
         frequency = $("#frequency").val().trim();
         console.log(frequency);
-        /////
-        //var tr = $("<tr>");
-        //console.log('firstShip is ' + firstShip);
-        var firstShipConverted = moment(firstShip, "hh:mm").subtract("1, years");
-        //console.log("first ship converted is " + firstShipConverted);
-        var currentTime = moment();
-        // the time difference between current time and the first train
-        var difference = moment().diff(moment(firstShipConverted), "minutes");
-        //console.log("the difference is " + difference);
-        var remainder = difference % frequency;
-        //console.log("the remainder is " + remainder);
-        minUntilShip = frequency - remainder;
-        //console.log("minutes until ship is " + minUntilShip);
-        moment(minUntilShip).minutes();
-        var nextArrival = moment().add(minUntilShip, "minutes").format("hh:mm a");
-        // tr.append("<td>" + nextArrival + "</td>");
-        // tr.append("<td>" + minUntilShip + "</td>");
-        /////
+
 
         if (ship !== '' && destination !== '' && firstShip !== '' && frequency !== '') {
 
@@ -57,8 +75,8 @@ $(function () {
                 destination: destination,
                 firstShip: firstShip,
                 frequency: frequency,
-                nextArrival: nextArrival,
-                minUntilShip: minUntilShip
+                firstShip: firstShip
+
             });
         } else {
             alert('Please make sure you fill out all fields. Thank you!');
